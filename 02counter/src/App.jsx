2df8a495ebbf7a5fafd88ddeb99436aba96a2495
Utkarsh <rajutkarsh580@gmail.com>
2024-01-24 22:51:52 +0530

    Tutorial

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -24,7 +24,7 @@ const removeValue=()=>{
     <h1>Chai aur react</h1>
     <h2>Counter value:{counter}</h2>
     <button 
-    onClick={addValue}
+    onClick={()=>setcounter(counter+1)}
     >Add value</button>
     <br />
     <button onClick={removeValue}>remove value</button>
